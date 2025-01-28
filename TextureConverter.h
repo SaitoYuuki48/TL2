e38b6883906461b099734b62753239e05f2ca175
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2025-01-28 15:12:46 +0900

    02_05

--- TextureConverter.h
+++ TextureConverter.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include "externals/DirectXTex/DirectXTex.h"
 
 class TextureConverter
 {
@@ -27,5 +28,29 @@ private:
 	/// <param name="mString">マルチバイト文字列</param>
 	/// <returns></returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string mString);
+
+	/// <summary>
+	/// フォルダとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
+	// 画像の情報
+	DirectX::TexMetadata metadata_;
+	// 画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+private:
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 };
 
